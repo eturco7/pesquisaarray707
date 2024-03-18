c654254a9872c05086caa727061edc23e98ec8a2
turco7 <enzoturcovic.tds1.senai@gmail.com>
2024-03-17 21:41:36 -0300

    first commit

--- some.js
+++ some.js
@@ -1,6 +1,6 @@
 //Some testa se pelo menos um elemento do array passa no teste implementado pela função fornecida.
 
-const array = [1, 2, 3, -4, -5];
+const trearray = [1, 2, 3, -4, -5];
 
-const algumNegativo = array.some(elemento => elemento < 0);
+const algumNegativo = trearray.some(elemento => elemento < 0);
 console.log(algumNegativo); 
